f2bdb02273dd4fa8cd09335b307b88a935c15492
Joan Puig Lledó <joanpulle@gmail.com>
2022-01-04 08:32:17 +0100

    save project on git

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,46 @@
+import Paciente from './Paciente'
+
+const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
+
+
+
+    return (
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
+            {
+                pacientes && pacientes.length ? (
+                    <>
+                        <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                        <p className="text-lg mt-5 text-center mb-10">
+                            Administra tus {''}
+                            <span className="text-indigo-600 font-bold">Pacientes y citas</span>
+                        </p>
+
+                        { pacientes.map((paciente) => {
+                            return (
+                                <Paciente
+                                    key={paciente.id}
+                                    paciente={paciente}
+                                    setPaciente={setPaciente}
+                                    eliminarPaciente={eliminarPaciente}
+                                />
+                            )
+                        })}
+                    </>
+                ) : (
+                    <>
+                        <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+                        <p className="text-lg mt-5 text-center mb-10">
+                            Empieza agregando pacientes {''}
+                            <span className="text-indigo-600 font-bold">y los podrás ver aquí</span>
+                        </p>
+                    </>
+                )
+
+            }
+
+
+        </div>
+    )
+}
+
+export default ListadoPacientes
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{html,jsx}',
+  './*.{html,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
